f4a2859ebdb4ea21f0049c5643c45c395c0f6d93
nkyomuhendo <nkyomuhendo@gmail.com>
2025-08-09 15:55:52 +0300

    Added Product Categories, Product Category Pages and filters

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -10,6 +10,20 @@ from django import forms
 
 
 # Create your views here.
+
+def category(request, foo):
+    # Replace Hyphens with Spaces
+    foo = foo.replace('-', ' ')
+    # Grab the category from the url
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+
+        return render(request, 'category.html', { 'products':products , 'category':category })
+
+    except:
+        messages.success(request, ("That Category Doesn't Exist..."))
+        return redirect('home')
 
 def product(request, pk):
     product = Product.objects.get(id=pk)
