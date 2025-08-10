ae98f253408d4927291f660cb69c11511346b9d7
nkyomuhendo <nkyomuhendo@gmail.com>
2025-08-10 17:19:33 +0300

    Added updating of Shopping cart Summary Page with items in cart

--- cart/views.py
+++ cart/views.py
@@ -5,7 +5,12 @@ from django.http import JsonResponse
 
 
 def cart_summary(request):
-    return render(request, "cart_summary.html", {})
+    # Get the cart
+    cart = Cart(request)
+
+    cart_products = cart.get_prods
+
+    return render(request, "cart_summary.html", {"cart_products": cart_products})
 
 
 
