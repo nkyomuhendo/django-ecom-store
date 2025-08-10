0b461227319358e657d729c801d011aef9d74831
nkyomuhendo <nkyomuhendo@gmail.com>
2025-08-10 16:02:46 +0300

    Added button and functionality to Add Products to cart and persist in user session, employed ajax and JsonResponse and JQuery minified from jQuery CDN

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 
 def cart_summary(request):
@@ -7,7 +10,21 @@ def cart_summary(request):
 
 
 def cart_add(request):
-    pass
+    # Get the cart
+    cart = Cart(request)
+    # test for POST
+    if request.POST.get('action') == 'post':
+        # Get Stuff
+        product_id = int(request.POST.get('product_id'))
+        # lookup product in the DB
+        product = get_object_or_404(Product, id=product_id)
+        
+        #Save to a session
+        cart.add(product=product)
+
+        # Return a response
+        response = JsonResponse({'Product Name: ': product.name})
+        return response
 
 def cart_delete(request):
     pass
