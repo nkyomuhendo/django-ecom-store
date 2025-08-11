e4dabfc77cb655ff957dd13d4df773d310ae423d
nkyomuhendo <nkyomuhendo@gmail.com>
2025-08-11 13:23:22 +0300

    Added Cart Totalling logic to the views and class file and enabled Total display on Cart Summary page

--- cart/cart.py
+++ cart/cart.py
@@ -30,6 +30,30 @@ class Cart():
 
         self.session.modified = True
 
+    
+    def cart_total(self):
+        # Get product IDs
+        product_ids = self.cart.keys()
+        # lookup those keys in our products database model
+        products = Product.objects.filter(id__in=product_ids)
+        # Get quantities
+        quantities = self.cart
+        # Start counting at 0
+        total = 0
+
+        for key, value in quantities.items():
+            # Convert key string into int so we can do math
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price * value)
+                    else:
+                        total = total + (product.price * value)
+        
+        return total
+
+
     def __len__(self):
         return len(self.cart)
     
--- cart/views.py
+++ cart/views.py
@@ -10,8 +10,9 @@ def cart_summary(request):
 
     cart_products = cart.get_prods
     quantities = cart.get_quants
+    totals = cart.cart_total()
 
-    return render(request, "cart_summary.html", {"cart_products": cart_products, "quantities":quantities})
+    return render(request, "cart_summary.html", {"cart_products": cart_products, "quantities":quantities, "totals": totals})
 
 
 
