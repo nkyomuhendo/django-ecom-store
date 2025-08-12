b2a19487c613ac628acda5a9d74b761c6059a863
nkyomuhendo <nkyomuhendo@gmail.com>
2025-08-12 15:40:25 +0300

    Created User Profile Page update_info using forms extending django User model to update and capture custom info from our App users

--- store/views.py
+++ store/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, redirect
-from .models import Product, Category
+from .models import Product, Category, Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
 
+
+def update_info(request):
+    if request.user.is_authenticated:
+        current_user = Profile.objects.get(user__id=request.user.id)
+        form = UserInfoForm(request.POST or None, instance=current_user)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your Info Has Been Updated!!!")
+            return redirect('home')
+        return render(request, "update_info.html", {'form': form})
+    
+    else:
+        messages.success(request, "You Must Be Logged In To Access That Page!!!")
+        return redirect('home')
 
 def update_password(request):
 
@@ -127,11 +142,10 @@ def register_user(request):
             #Log in user
             user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request, ("You have Registered successfully.. Welcome!!"))
-            return redirect('home')
+            messages.success(request, ("Username Created, Please Fill Out Your User Info Below...!!"))
+            return redirect('update_info')
         else:
             messages.success(request, ("Whoopsy!! There was a problem Registering your account... Please try again..!"))
-            return redirect('register')
-        
+            return redirect('register')       
     else:
         return render(request, 'register.html', {'form': form})
