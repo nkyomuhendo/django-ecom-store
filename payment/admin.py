2f4832629bac3665fa2134a07dd8c536243eea27
nkyomuhendo <nkyomuhendo@gmail.com>
2025-08-13 23:23:43 +0300

    Added models Order and OrderItem to handle order processing as precursor to checkout and payment processing

--- payment/admin.py
+++ payment/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import ShippingAddress
+from .models import ShippingAddress, Order, OrderItem
 
 
 # Regisger the model on the admin section page
 admin.site.register(ShippingAddress)
+admin.site.register(OrderItem)
+admin.site.register(Order)
