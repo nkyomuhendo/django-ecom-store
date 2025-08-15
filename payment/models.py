f7985a00ad64d4bec0ce3f5162b7ef21d28e1283
nkyomuhendo <nkyomuhendo@gmail.com>
2025-08-15 13:04:28 +0300

    Added Order Generation and creation capability utilizing user session objects to carry along information from different pages and forms

--- payment/models.py
+++ payment/models.py
@@ -39,7 +39,7 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     full_name = models.CharField(max_length=250)
     email = models.EmailField(max_length=250)
-    Shipping_address = models.TextField(max_length=15000)
+    shipping_address = models.TextField(max_length=15000)
     amount_paid = models.DecimalField(max_digits=7, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
 
--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,59 @@
 from django.shortcuts import render, redirect
 from cart.cart import Cart
 from payment.forms import ShippingForm, PaymentForm
-from payment.models import ShippingAddress
+from payment.models import ShippingAddress, Order, OrderItem
 from django.contrib import messages
+from django.contrib.auth.models import User
+
+
+def process_order(request):
+    if request.POST:
+        # Get the cart
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+
+        # Get Billing Info from the last page
+        payment_form = PaymentForm(request.POST or None)
+        #Get Shipping Session Data
+        my_shipping = request.session.get('my_shipping')
+
+        # Gather Order Info
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+      
+        # Create Shipping Address from session info
+        shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}"
+       
+        amount_paid = totals
+
+        # Create an Order
+        if request.user.is_authenticated:
+            # logged in
+            user = request.user
+            # Create an Order
+            create_order = Order(user=user, full_name = full_name, email=email, shipping_address= shipping_address, amount_paid=amount_paid)
+
+            create_order.save()
+
+            messages.success(request, "Order Placed!")
+            return redirect('home')
+                
+        else:
+            # not logged in
+           # Create an Order
+            create_order = Order(full_name = full_name, email=email, shipping_address= shipping_address, amount_paid=amount_paid)
+
+            create_order.save()
+
+            messages.success(request, "Order Placed!")
+            return redirect('home')
+
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('home')
 
 
 def billing_info(request):
@@ -13,6 +64,10 @@ def billing_info(request):
         cart_products = cart.get_prods
         quantities = cart.get_quants
         totals = cart.cart_total()
+
+        # Create a session with Shipping Info
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
 
         # Check to see if user is logged in
         if request.user.is_authenticated:
@@ -26,9 +81,9 @@ def billing_info(request):
             billing_form = PaymentForm()
             return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities":quantities, "totals": totals, "shipping_info": request.POST, 'billing_form': billing_form })
 
-        shipping_form = request.POST
+        # shipping_form = request.POST
 
-        return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities":quantities, "totals": totals, "shipping_form": shipping_form })
+        # return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities":quantities, "totals": totals, "shipping_form": shipping_form })
     else:
         messages.success(request, "Access Denied, this will be logged...!!")
         return redirect('home')
