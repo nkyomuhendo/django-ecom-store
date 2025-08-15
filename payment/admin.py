b3aea0828d860c41a3bc60792a4098a00f31b72b
nkyomuhendo <nkyomuhendo@gmail.com>
2025-08-15 18:07:00 +0300

    Added logic to empty cart after checkout and added Order Items Inline into the order section in the admin area

--- payment/admin.py
+++ payment/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
 from .models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 
 
 # Regisger the model on the admin section page
 admin.site.register(ShippingAddress)
 admin.site.register(OrderItem)
 admin.site.register(Order)
+
+
+# Create an Order Item inline
+
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+# Extend our Order Model
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    readonly_fields = ["date_ordered"]
+    fields = ["user", "full_name", "email", "shipping_address", "amount_paid", "date_ordered", "shipped" ]
+    inlines = [OrderItemInline]
+
+
+# Unregister Order Model
+admin.site.unregister(Order)
+
+# Re-Register Our Order AND OrderAdmin
+admin.site.register(Order, OrderAdmin)
--- payment/views.py
+++ payment/views.py
@@ -56,10 +56,16 @@ def process_order(request):
 
                 for key, value in quantities().items():
                     if int(key) == product.id:
-                        #quantity = value
+                       
                         # Create order item
                         create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
                         create_order_item.save()
+
+            # Delete Our cart!
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the key
+                    del request.session[key]
 
 
             messages.success(request, "Order Placed!")
@@ -94,6 +100,12 @@ def process_order(request):
                         # Create order item
                         create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
                         create_order_item.save()
+
+            # Delete Our cart!
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the key
+                    del request.session[key]
 
 
             messages.success(request, "Order Placed!")
